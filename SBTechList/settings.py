525e1a9790d13d3bf806744f85e2ec80a348f617
IndieTea <green.tea.sparkles@gmail.com>
2017-12-31 18:22:23 -0800

    Fix config

--- SBTechList/settings.py
+++ SBTechList/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = ENV['SECRET_KEY']
 #SECRET_KEY = '$o+gf!jt5md(9zh^ybs+c+e9n7l4t6e^25)b+zjfj=xj11smm4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = bool(ENV['DEBUG'])
 
 ALLOWED_HOSTS = []
 
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'SBTechList.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=ENV['DATABASE_URL']
     )
 }
 
