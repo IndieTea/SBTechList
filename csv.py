0cb79c369466bbab4bf8ddcfad321f049a45ad7a
IndieTea <green.tea.sparkles@gmail.com>
2018-01-05 13:28:57 -0800

    to display DB

--- csv.py
+++ csv.py
@@ -2,4 +2,4 @@ import csv
 with open( '___.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		print (row['organization'], row['cat'], row[''], row[''], row[''])
+		print (row['organization'], row['cat'], row['description'], row['website'], row['address'], row['city'], row['state'], row['zipcode'], row['employees'], row['founded'])
